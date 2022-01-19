d6ef7b7d5ee04fa8db3df98cefae3f5ac873bed6
mattszczerba <61034582+mattszczerba@users.noreply.github.com>
2022-01-19 09:47:42 -0500

    included missing dependencies

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Cart from "./Cart.js";
 
 function App() {
   const [cart, setCart] = useState(function () {
-    const savedString = localStorage.getItem("cart");
+    // const savedString = localStorage.getItem("cart");
     let savedCart = [];
     try {
       savedCart = JSON.parse(localStorage.getItem("cart")) || [];
--- src/ProductDetails.js
+++ src/ProductDetails.js
@@ -23,7 +23,7 @@ export default function ProductDetails(props) {
         setProduct(data);
       })
       .catch((error) => console.log("Could not load product details", error));
-  }, []);
+  }, [params]);
 
   return (
     <div className="product-details-layout">
